07bb93d88ec10a95ce9e5c7d35d03bcecbf5e272
Rafiki Mostafa <rafikimostafa@gmail.com>
2024-04-22 00:15:28 +0100

    Task.4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # Set up your client SSH configuration file
 # so that you can connect to a server without typing a password.
 
-file_line { 'Identity file':
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
-  }
-
-file_line { 'refuse to authenticate using a password':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PassewordAuthentication no'
-  }
+}
